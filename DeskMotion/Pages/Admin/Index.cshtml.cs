831f3ffb22f84d4fc0dac6b84936291671e9bee6
Mathias <tisen2013@gmail.com>
2024-12-01 20:34:52 +0100

    Added Role assignment
    Added:
    - Role assignment in User CRUD

--- DeskMotion/Pages/Admin/Index.cshtml.cs
+++ DeskMotion/Pages/Admin/Index.cshtml.cs
@@ -1,4 +1,4 @@
-// Copyright 2024 Web.Tech. Group17
+// Copyright 2024 PET Group16
 //
 // Licensed under the Apache License, Version 2.0 (the "License"):
 // you may not use this file except in compliance with the License.
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Text.Json;
 using DeskMotion.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace DeskMotion.Pages.Admin;
 
